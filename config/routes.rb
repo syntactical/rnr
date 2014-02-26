e797db88911364f6b169dc5d8eb2c2385745f375
michaelpai <mpai@thoughtworks.com>
2014-02-26 14:47:33 -0600

    Vivian/Michael Created VacationCalculator class using ActiveModel and changed view and controller accordingly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rnr::Application.routes.draw do
-  get "home/calculate_vacation_days"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get 'calculate', to: 'home#calculate', as: 'calculate'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
