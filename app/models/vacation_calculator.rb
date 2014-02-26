1858e221b356499da34ef1215c9a2dd202270309
michaelpai <mpai@thoughtworks.com>
2014-02-26 14:58:12 -0600

    add VacationCalculator class

---
+++ app/models/vacation_calculator.rb
@@ -0,0 +1,30 @@
+require 'active_model'
+
+class VacationCalculator
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :name ,:start_date, :end_date, :accrual_rate, :create
+
+  validates_length_of :name, :maximum => 10
+  validates :accrual_rate, numericality: {only_integer: true, greater_than_or_equal_to: 10}
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :validate_dates
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
+
+  def validate_dates
+    errors.add(:end_date, "can't be before the start date") if end_date < start_date
+  end
+
+end
