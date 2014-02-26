8a9dcef7d5f640349e03a8c52e63aa5dbb765305
sovivian <thoughtworker@VivianSo.local>
2014-02-26 17:59:30 -0600

    [Vivian and Michael] fixed unit test for home_controller_test to pass successfully

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   test "should get calculate_vacation_days" do
-    get (:calculate, {'vacation_calculator' => {name => 'Michael', start_date => nil, end_date => nil, accrual_rate => '10', create => 'blah'}}
+    get (:calculate, {'vacation_calculator' => {name => 'Michael', start_date => nil, end_date => nil, accrual_rate => '10', create => 'blah'}})
     assert_response :success
   end
 
