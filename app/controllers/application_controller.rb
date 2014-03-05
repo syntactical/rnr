87fccff7c32209651a4a4881d2f971c13d7d6374
syntactical <greg.dutcher@gmail.com>
2014-03-05 16:14:16 -0600

    change check for session information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   def require_login
-    if session[:userinfo]
-      redirect_to root_path
-    else
+    if session[:userinfo].nil?
+    #  redirect_to root_path
+    #else
       redirect_to '/auth/saml'
     end
   end
