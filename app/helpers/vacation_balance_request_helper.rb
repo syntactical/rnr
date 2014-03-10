98c94251ebbd63fc4e3e456e7d1029f0ba01cff6
syntactical <greg.dutcher@gmail.com>
2014-03-10 17:06:29 -0500

    hard code RNR sandbox salesforce URL

--- app/helpers/vacation_balance_request_helper.rb
+++ app/helpers/vacation_balance_request_helper.rb
@@ -1,6 +1,6 @@
 module VacationBalanceRequestHelper
   def client
-    @client ||= Restforce.new instance_url:  'https://test.salesforce.com/services/oauth2/token',
+    @client ||= Restforce.new instance_url:  'https://test.salesforce.com/services/oauth2/authorize',
                           oauth_token:   session['token']
   end
 end
