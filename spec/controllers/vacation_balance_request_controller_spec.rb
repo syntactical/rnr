255f276dccaabe609467746ab7ed90432a6aa4bb
sovivian <thoughtworker@VivianSo.local>
2014-03-12 11:35:53 -0500

    [Vivian and Michael] fixed rspec test for vacation_balance_request_controller to make puts work

--- spec/controllers/vacation_balance_request_controller_spec.rb
+++ spec/controllers/vacation_balance_request_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe VacationBalanceRequestController do
   context 'get index' do
     it 'should instantiate a new empty VacationBalanceRequest' do
+      session[:userinfo] = 'blah'
       get :index
       assigns(:vacation_balance_request).should be_a(VacationBalanceRequest)
     end
