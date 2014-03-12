a219356c927c9f8dacae7cf9ddaf0eb31fc6e4f7
michaelpai <mpai@thoughtworks.com>
2014-03-12 14:07:52 -0500

    [Michael and Vivian] Changed OmniAuth consumer service url to https instead of http.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
-           :assertion_consumer_service_url     => "http://rnr-rails.herokuapp.com/auth/saml/callback",
+           :assertion_consumer_service_url     => "https://rnr-rails.herokuapp.com/auth/saml/callback",
            :issuer                             => "RnR Test",
            :idp_sso_target_url                 => "https://thoughtworks.oktapreview.com/app/template_saml_2_0/k1w3t2c2INSPQMHOLJIU/sso/saml",
            :idp_cert                           => ENV["IDP_CERT"],
